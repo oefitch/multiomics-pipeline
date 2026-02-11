0f52395cc26216952802c77504f499c10eeeb368
Olivia Fitch <oefitch19@gmail.com>
2026-02-11 14:37:03 -0500

    last updated 2/11/26

--- Fitch_full_ATAC_pipeline.sh
+++ Fitch_full_ATAC_pipeline.sh
@@ -1,9 +1,6 @@
 #bbduk
 module load BBMap/39.01-GCC-12.3.0
 
-#parallel
-module load parallel/20230722-GCCcore-12.3.0
-
 #bowtie
 module load Bowtie2/2.5.1-GCC-12.3.0
 
@@ -24,31 +21,32 @@ sample_names="" #replace with your own sample names
 
 #### Trim Reads ####
 #NOTE: with new package (faster than trimmomatic) in parallel (this is after re-naming read files from novogene)
-parallel 'bbduk.sh in={}_1.fq.gz in2={}_2.fq.gz out={}_pe_bbtrimmed_1.fq out2={}_pe_bbtrimmmed_2.fq outs={}_se_bbtrimmed.fq ref=adapter_contamination_sequences.fasta ktrim=r minlength=25' ::: $sample_names #this list is what will go in {} one at a time
+bbduk.sh in={}_1.fq.gz in2={}_2.fq.gz out={}_pe_bbtrimmed_1.fq out2={}_pe_bbtrimmmed_2.fq outs={}_se_bbtrimmed.fq ref=adapter_contamination_sequences.fasta ktrim=r minlength=25
 
 #### Map to Genome ####
 #NOTE: need the genome file and index in your directory
-parallel 'bowtie2  --very-sensitive  -x gar_index  -1 {}_pe_bbtrimmed_1.fq -2 {}_pe_bbtrimmmed_2.fq -S {}.sam' ::: $sample_names
+bowtie2  --very-sensitive  -x gar_index  -1 {}_pe_bbtrimmed_1.fq -2 {}_pe_bbtrimmmed_2.fq -S {}.sam
 
 #### Convert Sam to Bam ####
-parallel 'samtools view -u {}.sam | samtools sort -n -o {}_sorted.bam' $sample_names
+
+samtools view -u {}.sam | samtools sort -n -o {}_sorted.bam
 
 #after this you should have your cleaned and sorted bam files so you can now clean them up further 
 
 #### Remove Mitochondrial Reads #### 
 #NOTE: sometimes you have to sort again after this
-parallel 'samtools view -h  {}_sorted.bam  |  python path/removeChrom.py - - MT  |  samtools view -b | samtools sort - > {}_sorted_mitout.bam' ::: $sample_names
+samtools view -h  {}_sorted.bam  |  python path/removeChrom.py - - MT  |  samtools view -b | samtools sort - > {}_sorted_mitout.bam
 
 #### Remove PCR Duplicates #### 
 #NOTE: sometimes you have to sort again after this
-parallel 'java -jar path/picard.jar MarkDuplicates I={}_sorted_mitout.bam O={}_sorted_mitout_dupout.bam M={}_dups.txt REMOVE_DUPLICATES=true' ::: $sample_names
+java -jar path/picard.jar MarkDuplicates I={}_sorted_mitout.bam O={}_sorted_mitout_dupout.bam M={}_dups.txt REMOVE_DUPLICATES=true
 
 #### Remove non-unique reads ####
 #NOTE: sometimes you have to sort again after this, and sometimes samtools index doesnt like sort with "-n" so run w/o
-parallel 'samtools view -b  -q 10  {}_sorted_mitout_dupout_sort.bam | samtools sort -n -o  {}_sorted_mitout_dupout_uniq.bam' ::: $sample_names
+samtools view -b  -q 10  {}_sorted_mitout_dupout_sort.bam | samtools sort -n -o  {}_sorted_mitout_dupout_uniq.bam
 
 #### Index final bam files ####
-parallel 'samtools index {}_sorted_mitout_dupout_uniq.bam' ::: $sample_names
+samtools index {}_sorted_mitout_dupout_uniq.bam
 
 #NOTE: These next two can be run at the same time
 
@@ -57,13 +55,14 @@ cd path #navigate to the environment where conda is installed
 conda activate atac #activate the conda environment where you installed bamcoverage 
 #### Convert bam to bigwig ####
 cd path #navigate to the directory where you want to work
-parallel 'bamCoverage -b {}_sorted_mitout_dupout_uniq.bam -of bigwig --normalizeUsing BPM --ignoreForNormalization MT --effectiveGenomeSize 345577542 -o {}_23.bw' ::: $sample_names
+SIZE_in_BP= #size of your genome in base-pairs 
+bamCoverage -b {}_sorted_mitout_dupout_uniq.bam -of bigwig --normalizeUsing BPM --ignoreForNormalization MT --effectiveGenomeSize $SIZE_in_BP -o {}_final.bw
 conda deactivate
 
 
 #### Call Peaks with MACS #### 
 #NOTE: this will make a new folder, so make sure it's putting it where you want it
 #NOTE: sometimes MACS doesn't want to run in parallel (idk why)
-
-parallel 'macs2 callpeak -t {}_sorted_mitout_dupout_uniq.bam -f BAMPE -g 345577542 -n {}_23 -B -q 0.05 -s 75 --call-summits --outdir MACS2_{}_23' ::: $sample_names
+SIZE_in_BP= #size of your genome in base-pairs 
+macs2 callpeak -t {}_sorted_mitout_dupout_uniq.bam -f BAMPE -g $SIZE_in_BP  -n {}_23 -B -q 0.05 -s 75 --call-summits --outdir MACS2_{}
 
